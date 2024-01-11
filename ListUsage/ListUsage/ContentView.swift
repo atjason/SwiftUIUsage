0eab79a31d9189552e39aa13efbefcaf1f62535c
Jason <quietjason@gmail.com>
2024-01-11 21:48:25 +0800

    Add list usage.

--- ListUsage/ListUsage/ContentView.swift
+++ ListUsage/ListUsage/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var users = ["Tom", "Jim", "Emily"]
+  
+  var body: some View {
+    NavigationStack {
+      List($users, id: \.self, editActions: .all) { $user in
+        Text(user)
+      }
+      .navigationTitle("Users")
+      .toolbar() {
+        EditButton()
+      }
+            
+      List {
+        ForEach($users, id: \.self) { $user in
+          Text(user)
         }
-        .padding()
+        .onMove(perform: move)
+        .onDelete(perform: delete)
+      }
     }
+  }
+  
+  func move(from source: IndexSet, to destination: Int) {
+    withAnimation {
+      users.move(fromOffsets: source, toOffset: destination)
+    }
+  }
+  
+  func delete(at offsets: IndexSet) {
+    withAnimation {
+      users.remove(atOffsets: offsets)
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
